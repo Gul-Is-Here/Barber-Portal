5c020fda4667761ee01d6ac87f3cebf4979a1657
Gul-Is-Here <gulfarazahmed58@gmail.com>
2024-05-18 00:38:30 +0500

    'minor bug fix'

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 const greenColor = Color.fromRGBO(107, 153, 64, 6);
 const darkBlueColor = Color.fromRGBO(18, 77, 133, 1);
 const darkGreenColor = Color.fromRGBO(14, 114, 59, 0.925);
+
+class AppColors {
+  final appBarColor = AppBarTheme(
+    backgroundColor: Color.fromRGBO(14, 114, 59, 0.925),
+  );
+}
